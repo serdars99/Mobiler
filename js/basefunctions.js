5f0939057d0203e3428f679e7d1958b179c533eb
serdars99 <serdarsaracoglu@gmail.com>
2013-12-30 10:01:30 +0200

    sdd

--- js/basefunctions.js
+++ js/basefunctions.js
@@ -1,5 +1,5 @@
-﻿//var siteurl = "http://www.bahisor.com/";
-var siteurl = "http://localhost:26087/";
+﻿var siteurl = "http://www.bahisor.com/";
+//var siteurl = "http://localhost:26087/";
 var apiurl = siteurl + "mobile/";
 var defaulttext = 'Yazmak için tıklayın...';
 var chatcheckdate = moment(new Date()).subtract('days', 1);
